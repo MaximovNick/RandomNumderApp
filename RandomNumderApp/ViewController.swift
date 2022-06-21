4961ac59d9beb7488551ceb5f7c0e6ef850ea1fc
Nikolai Maximov <kolkamaximov@gmail.com>
2022-06-21 10:34:59 +0300

    Initial Commit

---
+++ RandomNumderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomNumderApp
+//
+//  Created by Nikolai Maksimov on 21.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
